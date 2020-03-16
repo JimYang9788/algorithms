9f5c743ce786acfb571c7e57cc3a07c1f4062927
Jim Yang <yang9788@yahoo.com>
2020-03-16 10:02:18 -0600

    Daily Dose of Algo Problems

---
+++ Markdown/primitive-types/demo.py
@@ -0,0 +1,12 @@
+
+def count_bits (x: int) -> int:
+    num_bits = 0
+    while x:
+        num_bits += x & 1 
+        x >>= 1 
+    return num_bits 
+
+    
+
+if __name__ == "__main__":
+    print (count_bits(3))
