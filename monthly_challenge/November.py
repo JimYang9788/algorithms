cfa891972fd238d433c58d2d07d1c715a890fce5
Jim Yang <yang9788@yahoo.com>
2020-11-17 22:15:20 -0500

    daily dose

--- monthly_challenge/November.py
+++ monthly_challenge/November.py
@@ -234,6 +234,48 @@ class Solution:
             if dummy2 in hash_set:
                 return dummy2 
             dummy2 = dummy2.next
-            
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """ 
+        if not headA or not headB: return None
+
+        p = headA
+        while p.next:
+            p = p.next
+        mark = p
+        #make a cycled list
+        mark.next = headA
+
+        rst = None
+        p1 = headB
+        p2 = headB
+        while p1 and p2:
+            p1 = p1.next
+            p2 = p2.next
+            if p2: p2 = p2.next
+            if p1 == p2: break
+
+        if p1 and p2 and p1 == p2:
+            p1 = headB
+            while p1 != p2:
+                p1 = p1.next
+                p2 = p2.next
+            rst = p1
+
+        #unmake the cycle
+        mark.next = None
+
+        return rst
+
+
 if __name__ == '__main__':
     zeros = Solution.setZeroes([[1,1,1],[1,0,1],[1,1,1]])
