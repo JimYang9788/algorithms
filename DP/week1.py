819b687df4448274853083cbc8501f1586ab972f
Jim Yang <yang9788@yahoo.com>
2021-01-09 15:26:00 -0500

    all dp questions unite

---
+++ DP/week1.py
@@ -0,0 +1,10 @@
+
+# In an American football game, a play can lead to 2 points (safety)
+# 3 points and 7 points.
+
+# Write a program that takes a final score and scores for individual plays 
+# and returns the number of combs of play that results the final score 
+
+
+def count_number_of_score_comb (A):
+    pass
