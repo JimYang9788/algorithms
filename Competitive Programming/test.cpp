f8a711ce5db08b020078b7b73ae24166c7a2c3b9
Jim Yang <yang9788@yahoo.com>
2021-05-21 11:35:36 -0400

    flush action

--- Competitive Programming/test.cpp
+++ Competitive Programming/test.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(){
     int a, b;
     string x;
-    cin >> a >> b >> x;
+    cin >> a >> b >> x >> "flush action";
+
     cout << a << " " << b << " " << x << "\n"<<flush;
     return 0;
-}
+} 
