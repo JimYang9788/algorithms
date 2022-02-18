b71fc2cf13f8791a348d472409cbe6ee612e11fe
Jim Yang <yang9788@yahoo.com>
2022-02-18 00:20:04 -0500

    adding copy assignment and move assignemnt

--- C++/share_ptr/share_pointer.h
+++ C++/share_ptr/share_pointer.h
@@ -3,9 +3,88 @@
 #include <iostream>
 
 template <typename T>
-class SharePointer{
-    
-}
+class SharePointer
+{
+public:
+    // Constructor
+    SharePointer<T>(T *ptr) : d_ptr(ptr), d_count(nullptr)
+    {
+        std::cout << "Constructor Called" << std::endl;
+        d_count = new size_t;
+        *d_count = 1;
+    }
+    SharePointer<T>() : d_ptr(nullptr), d_count(nullptr)
+    {
+        d_count = new size_t;
+        *d_count = 1;
+    }
+
+    // Copy Constructor
+    SharePointer<T>(SharePointer &sp) : d_ptr(sp.get_ptr()), d_count(sp.get_d_count())
+    {
+        (*d_count)++;
+        std::cout << "Copy Constructor Called" << std::endl;
+    }
+
+    // Move Constructor
+    SharePointer<T>(SharePointer &&sp) : d_ptr(sp.get_ptr()), d_count(sp.get_d_count())
+    {
+        (*d_count)++;
+        std::cout << "Move Constructor Called" << std::endl;
+    }
+
+    // Copy Assignment
+    SharePointer<T>& operator=(SharePointer &sp)
+    {
+        std::cout << "Copy Assignment" << std::endl;
+        (*d_count)--;
+        d_ptr = sp.get_ptr();
+        d_count = sp.get_d_count();
+        (*d_count)++;
+        return *this;
+    }
+
+
+    // Copy Assignment
+    SharePointer<T>& operator=(SharePointer &&sp)
+    {
+        std::cout << "Move Assignment" << std::endl;
+        (*d_count)--;
+        d_ptr = sp.get_ptr();
+        d_count = sp.get_d_count();
+        (*d_count)++;
+        return *this;
+    }
+
+    // Deleter
+    ~SharePointer()
+    {
+        (*d_count)--;
+        std::cout << "Destructor Called. Count Remaining: " << *d_count << std::endl;
+
+        if (*d_count == 0)
+        {
+            delete d_ptr;
+            delete d_count;
+        }
+    }
+
+    T *get_ptr()
+    {
+        return d_ptr;
+    }
+    size_t *get_d_count()
+    {
+        return d_count;
+    }
+    size_t use_count()
+    {
+        return *d_count;
+    }
+
+private:
+    T *d_ptr;        // Underlying Pointer
+    size_t *d_count; //
+};
 
 #endif
-
--- C++/share_ptr/share_ptr.m.cpp
+++ C++/share_ptr/share_ptr.m.cpp
@@ -1,6 +1,13 @@
 #include "share_pointer.h"
-
+#include <utility>
+#include <memory>
 #include <iostream>
-    int main (){
-        std::cout<< "hello world" << std::endl;
-    }
+
+int main()
+{
+    // SharePointer<int> sp2(new int);
+    SharePointer<int> sp1(new int);
+    SharePointer<int> sp3(std::move(sp1));
+    sp1 = sp3;
+    sp1 = std::move (sp3);
+}
